959e76cc3d86978e115fff2ac2aa901f2efb5d3d
Minitte <arcentalminitte@gmail.com>
2017-08-16 19:01:25 -0700

    scene2d replaces ashley

--- core/src/com/ozma/sameW/woo1/Core.java
+++ core/src/com/ozma/sameW/woo1/Core.java
@@ -1,10 +1,14 @@
 package com.ozma.sameW.woo1;
 
-import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.Contact;
@@ -12,11 +16,23 @@ import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.ozma.sameW.woo1.character.Player;
 
 public class Core extends Game {
 	
 	// render
+	private static float ppm = 2f;
+	private static float mpp = 1f/ppm;
+	
 	private SpriteBatch batch;
+	public OrthographicCamera camera;
+	
+	private TiledMap map;
+	private TiledMapRenderer mapRenderer;
+	
+	private Stage stage;
 	
 	// box2d
 	public static final float TIME_STEP = 0.01f;
@@ -25,17 +41,25 @@ public class Core extends Game {
 	private float accumulator = 0;
 	private Box2DDebugRenderer debugRenderer;
 	
-	// Ashley
-	private PooledEngine engine;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Gdx.graphics.getWidth() * mpp, Gdx.graphics.getHeight() * mpp);
+		
+		// map
+		map = new TmxMapLoader().load("assets/map/test.tmx");
+		mapRenderer = new OrthoCachedTiledMapRenderer(map);
+		mapRenderer.setView(camera);
 		
 		// box2d
 		world = new World(new Vector2(0, 0), true);
 		debugRenderer = new Box2DDebugRenderer();
 		setUpContactListener();
+		
+		// scene2d
+		stage = new Stage(new ScreenViewport(camera), batch);
+		stage.addActor(new Player(new Vector2(100f, 100f)));
 	}
 
 	@Override
@@ -47,7 +71,12 @@ public class Core extends Game {
 		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		// draw stuff
+		
+		camera.update();
+		mapRenderer.render();
+		debugRenderer.render(world, camera.combined);
+		
+		
 		batch.end();
 	}
 	
