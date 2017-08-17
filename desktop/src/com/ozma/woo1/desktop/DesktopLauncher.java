2bec7d0ffce2e540ff19368e5218ed7b1af526d7
Minitte <arcentalminitte@gmail.com>
2017-08-16 17:53:07 -0700

    adjusted resolution

--- desktop/src/com/ozma/woo1/desktop/DesktopLauncher.java
+++ desktop/src/com/ozma/woo1/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package com.ozma.woo1.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.ozma.woo1.MyGdxGame;
+import com.ozma.sameW.woo1.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
