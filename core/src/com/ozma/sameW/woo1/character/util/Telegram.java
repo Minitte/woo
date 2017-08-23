2f6b89a0ff6e9094443d7ac67718e25f106ac911
Minitte <arcentalminitte@gmail.com>
2017-08-23 16:15:15 -0700

    Removed telegram

--- core/src/com/ozma/sameW/woo1/character/util/Telegram.java
+++
@@ -1,8 +0,0 @@
-package com.ozma.sameW.woo1.character.util;
-
-import com.ozma.sameW.woo1.character.Character;
-
-public interface Telegram {
-	
-	public void processMessage(Character sender, int msg);
-}
