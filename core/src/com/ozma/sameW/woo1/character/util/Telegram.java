c19650d12da61311ef63737007862e91ee4884f7
Minitte <arcentalminitte@gmail.com>
2017-08-23 16:03:42 -0700

    Wrong character

--- core/src/com/ozma/sameW/woo1/character/util/Telegram.java
+++ core/src/com/ozma/sameW/woo1/character/util/Telegram.java
@@ -1,5 +1,7 @@
 package com.ozma.sameW.woo1.character.util;
 
+import com.ozma.sameW.woo1.character.Character;
+
 public interface Telegram {
 	
 	public void processMessage(Character sender, int msg);
