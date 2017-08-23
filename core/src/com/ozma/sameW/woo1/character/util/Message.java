f6b12e478829c280371556f3f1a98cd7a01a33e4
Minitte <arcentalminitte@gmail.com>
2017-08-23 16:15:47 -0700

    Message Types

---
+++ core/src/com/ozma/sameW/woo1/character/util/Message.java
@@ -0,0 +1,15 @@
+package com.ozma.sameW.woo1.character.util;
+
+public enum Message {
+	
+	TOUCH(1);
+	
+	public final int id;
+
+	/**
+	 * @param id
+	 */
+	private Message(int id) {
+		this.id = id;
+	}
+}
