e829769484a4c432079e0b86ab2dbd67bb068816
Minitte <arcentalminitte@gmail.com>
2017-08-23 17:13:54 -0700

    unitcircle stuff

--- core/src/com/ozma/sameW/woo1/util/Constants.java
+++ core/src/com/ozma/sameW/woo1/util/Constants.java
@@ -8,4 +8,33 @@ public class Constants {
 	 * m * ppm = px
 	 */
 	public final static float PPM = 5f;
+	
+	public enum UnitCircle {
+		
+		EAST 		(0),
+		SOUTH_EAST 	(Math.PI * 7 / 4),
+		SOUTH 		(Math.PI * 3 / 2),
+		SOUTH_WEST 	(Math.PI * 5 / 4),
+		WEST 		(Math.PI),
+		NORTH_WEST 	(Math.PI * 3 / 4),
+		NORTH 		(Math.PI / 2),
+		NORTH_EAST 	(Math.PI / 4),
+		HALF_PI 	(Math.PI / 2);
+		
+		public final float value;
+
+		/**
+		 * @param value
+		 */
+		private UnitCircle(float value) {
+			this.value = value;
+		}
+		
+		/**
+		 * @param value
+		 */
+		private UnitCircle(double value) {
+			this.value = (float) value;
+		}
+	}
 }
